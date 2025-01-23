62f75f18ff65fee618c31cad1f9c5db992f582d5
antulek <antonikarolmalak@gmail.com>
2025-01-23 19:02:44 +0100

    spring cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-// main.js or main.ts
+// bootstrap and font-awesome icons imports
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+// compnents imports
+import TestComponent from "./components/TestComponent.vue";
 </script>
 
 <template>
-  <div class="fa-spin">
-    <h3>Hello world!</h3>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <TestComponent>
+
+  </TestComponent>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      greeting: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <h3 class="greeting">{{ greeting }}</h3>
+
+  <div class="card">
+    <div class="card-header">
+      Header
+    </div>
+    <div class="card-body">
+      Body
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  h3 {
+    color: black;
+    font-weight: bold;
+    background-color: yellow;
+  }
+</style>
