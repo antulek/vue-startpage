212c890ec1b45dca23d56e17cc29fcba2e0a9e9b
antulek <antonikarolmalak@gmail.com>
2025-04-07 17:26:01 +0200

    Added hot-reload script

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import hmr from './src/vite-hmr.js';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), hmr()],
   base: "/vue-startpage/",
   resolve: {
     alias: {
