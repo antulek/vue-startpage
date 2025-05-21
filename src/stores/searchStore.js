62c84b2cfdb13ba49a4245813ac3f3087e77b26b
antulek <antonikarolmalak@gmail.com>
2025-05-21 19:04:39 +0200

    Deleted part of code that added http and maybe www to address. Search addreses are always without them, so only adding http is necessary

--- src/stores/searchStore.js
+++ src/stores/searchStore.js
@@ -14,22 +14,16 @@ export const useSearchStore = defineStore('searchStore',{
             let destination = "";
             let parameters = lucky ? "?btnI=1&q=" : "?q=";
             if(query && !site){
-                destination = "http://www."+"google.com/search"+parameters+encodeURI(query);
+                destination = "google.com/search"+parameters+encodeURI(query);
             }else if(!query && site && site.address){
                 destination = site.address;
             }else if(query && site && site.address && !site.search_address){
-                destination = "http://www."+"google.com/search"+parameters+encodeURI("site:"+site.address+" "+query);
+                destination = "google.com/search"+parameters+encodeURI("site:"+site.address+" "+query);
             }else if(query && site && site.address && site.search_address){
                 destination = site.search_address+encodeURI(query);
             }
 
-            if(!destination.includes(':') && !destination.includes('www.'))
-                destination = 'http://www.'+destination;
-            else if(destination.includes(':') && !destination.includes('www.'))
-                destination = destination.replace('://','://www,');
-            else if(!destination.includes(':') && destination.includes('www.'))
-                destination = 'http://'+destination;
-
+            destination = 'http://'+destination;
 
             if(query || site){
                 if(newTab)
